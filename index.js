603bf8bfb535a660c63de5b6b5aa1fa20b8658f1
Teejay Villaluz <traxxjester25@gmail.com>
2022-09-20 16:46:00 +0800

    edit port

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const dotenv = require('dotenv');
 const path = require('path');
 const cors = require('cors');
 
-const port = 3001;
 const hostname = 'localhost';
 
 dotenv.config({path: './.env'});
+
+const port = process.env.PORT;
 
 const app = express();
 
@@ -27,7 +28,7 @@ const db = mysql.createConnection({
 // /users/register
 app.use('/users', require('./routes/user'));
 
-app.listen(port, hostname, () => {
+app.listen(port, () => {
     console.log(`Server started at http://${hostname}:${port}`);
     db.connect( (err) => {
         if(err) {
